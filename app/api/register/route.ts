f65455471b17db74e42fcd170dfd09f278c23ade
krishnash47 <166825864+krishnash47@users.noreply.github.com>
2024-09-12 22:32:39 +0530

    Add files via upload

---
+++ app/api/register/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// Hashing function from bcrypt
+import { hash, hashSync } from 'bcryptjs';
+import { isUserExists, registerNewUser } from '@/utils/api-methods';
+
+// API helper functions
+
+export async function POST(request: NextRequest) {
+  try {
+    // TODO: Body Parser with the help of zod
+    const { userName, userEmail, userPassword } = await request.json();
+
+    // Check if user already exists
+    const userData = await isUserExists(userEmail);
+    if (userData) {
+      return NextResponse.json({
+        data: null,
+        message: 'User already exists',
+      });
+    }
+
+    const hashedPassword = hashSync(userPassword, 10);
+
+    const res = await registerNewUser({
+      data: {
+        name: userName,
+        email: userEmail,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({
+      data: res,
+      message: 'User registered successfully',
+    });
+  } catch (error) {
+    return NextResponse.json({
+      data: null,
+      message: 'Error registering user',
+    });
+  }
+}
